0cdb6cc10f902958e96188fe13c23625eb624b96
diydriller <dhrhd080@naver.com>
2024-03-20 02:42:16 +0900

    refactor: 활성화된 유저만 조회하도록 수정

--- src/main/java/com/gridge/server/dataManager/member/MemberRepository.java
+++ src/main/java/com/gridge/server/dataManager/member/MemberRepository.java
@@ -2,6 +2,7 @@ package com.gridge.server.dataManager.member;
 
 import com.gridge.server.service.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,8 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByNickname(String nickname);
+    @Query("SELECT m FROM Member m WHERE m.nickname = :nickname AND m.state = 'ACTIVATED'")
     Optional<Member> findByNickname(String nickname);
+    @Query("SELECT m FROM Member m WHERE m.id = :id AND m.state = 'ACTIVATED'")
+    Optional<Member> findById(Long id);
 }
